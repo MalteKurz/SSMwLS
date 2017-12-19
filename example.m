7efcc979a2253991622637be141b3701de262f87
Malte Kurz <malte.kurz@stat.uni-muenchen.de>
2017-12-19 17:50:14 +0100

    multidimensional example added

--- example.m
+++ example.m
@@ -1,18 +1,30 @@
-nObs = 10000;
+% One dimensional state
+nObs = 1000;
 
 
 A = 0.5;
-C = [1, 0.5];
-R = [0.5, 1];
+C = [1, 0];
+R = [0, 1];
 D1 = 1;
 D2 = 0.5;
+
+% Three-dim state, two-dim observations, five-dim disturbances
+A = diag([0.8,0.2,0.1]);
+C = [diag([1, 0.9, 1.4]), zeros(3,2)];
+R = [zeros(2,3), diag([0.8, 1.1])];
+D1 = [1, 0.2, 0.1;...
+    0.7, 0.9, 0.2];
+D2 = [0.5, 0.1, 0.05;...
+    0.9,0.05, 0.2];
+
+
 
 % initialize
 dimDisturbance = size(R,2);
 dimState       = size(A,1);
 dimObs         = size(D1,1);
-Z              = nan(nObs, 1);
-X              = nan(nObs, 1);
+Z              = nan(nObs, dimObs);
+X              = nan(nObs, dimState);
 
 a_0_0 = zeros(dimState, 1);
 CC = C*C';
@@ -23,8 +35,8 @@ X(1,:) = mvnrnd(a_0_0, P_0_0);
 u = randn(nObs, dimDisturbance);
 
 for iObs = 1:nObs
-    X(iObs+1,:) = A * X(iObs,:)' + C * u(iObs,:)';
-    Z(iObs) = D1 * X(iObs+1,:)' + D2 * X(iObs,:)' + R * u(iObs,:)';
+    X(iObs+1, :) = A * X(iObs,:)' + C * u(iObs,:)';
+    Z(iObs, :)   = D1 * X(iObs+1,:)' + D2 * X(iObs,:)' + R * u(iObs,:)';
     
 end
 
@@ -51,7 +63,7 @@ D1_bar = [D1, D2];
 D2_bar = zeros(dimObs, 2*dimState);
 
 smoothStateAugmented = smooth(A_bar,C_bar,D1_bar,D2_bar,R,Z');
-smoothStateAugmented = smoothStateAugmented(:,2:end)';
+smoothStateAugmented = smoothStateAugmented(:,:)';
 
 
 
